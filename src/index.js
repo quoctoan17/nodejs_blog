a70972cfd48fe9c8d2607dc85cede5d6edc9e56d
quoctoan17 <toanlenguyen1705@gmail.com>
2025-12-07 20:15:16 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -5,24 +5,31 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+const route = require('./routes');
 
+//middleware    
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//http logger
+// app.use(morgan('combined'));
+
+//template engine
 app.engine('hbs', handlebars.engine({
         extname: '.hbs'
 }));
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render(`home`);
-});
+// Home, search, contact
 
-app.get('/news', (req, res) => {
-    res.render(`news`);
-});
+//route init
+route(app);
 
 app.listen(port, () => 
     console.log(`Example app listening at http://localhost:${port}`));
