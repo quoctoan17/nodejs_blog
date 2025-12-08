60c84ed0da9fc796d16500c47429a69e1286c183
quoctoan17 <toanlenguyen1705@gmail.com>
2025-12-08 20:19:31 +0700

    husky

--- src/index.js
+++ src/index.js
@@ -29,13 +29,12 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-    app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Home, search, contact
 
 //route init
-    route(app);
-
-            app.listen(port, () =>
-                console.log(`Example app listening at http://localhost:${port}`),
-               );
+route(app);
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
