6794805e93371cd241ddb074823e6547a135ed6d
quoctoan17 <toanlenguyen1705@gmail.com>
2025-12-08 20:25:38 +0700

    husky

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
 route(app);
+
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`),
 );
